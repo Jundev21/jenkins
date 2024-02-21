91f1f45bf3becb1c35593e1885b4c91ac0374cd3
jun <jundev21@gmail.com>
2024-02-21 14:21:52 +0900

    Initial commit

---
+++ src/test/java/com/practice/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
